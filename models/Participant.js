113e52d7d21129b4c456cc8c90f13a740d924326
Thiago Souza <153232481+thiagosouza28@users.noreply.github.com>
2025-01-19 16:30:02 -0300

    Update Participant.js

--- models/Participant.js
+++ models/Participant.js
@@ -1,15 +1,47 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const participantSchema = new mongoose.Schema({
-    id_participante: { type: String, required: true, unique: true },
-    nome: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    nascimento: { type: Date, required: true },
-    idade: { type: Number, required: true },
-    igreja: { type: mongoose.Schema.Types.ObjectId, ref: 'Igreja', required: true }, // Assuming you have an 'Igreja' model
-    data_inscricao: { type: Date, default: Date.now },
-    data_confirmacao: Date,
-    id_usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario' } // Assuming you have a 'Usuario' model
+const participantSchema = new Schema({
+    id_participante: { // ID personalizado, por exemplo, DI20230001
+        type: String,
+        required: true,
+        unique: true
+    },
+    nome: {
+        type: String,
+        required: [true, 'O nome é obrigatório'],
+        trim: true,
+        maxlength: [100, 'O nome deve ter no máximo 100 caracteres']
+    },
+    nascimento: {
+        type: Date,
+        required: [true, 'A data de nascimento é obrigatória']
+    },
+    idade: {
+        type: Number,
+        required: [true, 'A idade é obrigatória'],
+        min: [0, 'A idade deve ser maior ou igual a 0']
+    },
+    igreja: {
+        type: String,
+        trim: true,
+        required: [true, 'O nome da igreja é obrigatório']
+    },
+    email: {
+        type: String,
+        required: [true, 'O email é obrigatório'],
+        lowercase: true,
+        trim: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Por favor, insira um email válido']
+    },
+    data_inscricao: {
+        type: Date,
+        default: Date.now
+    },
+    data_confirmacao: {
+        type: Date,
+        default: null // Inicialmente, a data de confirmação é nula
+    }
 });
 
 module.exports = mongoose.model('Participant', participantSchema);
