c72ca7f8fe3a836b180138a1d2402789c5c342d1
Thiago Souza <153232481+thiagosouza28@users.noreply.github.com>
2025-01-19 16:15:30 -0300

    Update participantRoutes.js

--- routes/participantRoutes.js
+++ routes/participantRoutes.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const participantController = require('../controllers/participantController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
-//Rotas para participantes
-router.post('/', participantController.createParticipant); // Rota para criar um novo participante
-router.post('/public', participantController.createPublicParticipant); // Rota pública para criar participantes
-router.get('/', participantController.getAllParticipants); // Rota para obter todos os participantes
-router.get('/:id_participante', participantController.getParticipantById); // Rota para obter um participante pelo ID
-router.put('/:id_participante', participantController.updateParticipant); //Rota para atualizar um participante
-router.put('/:id_participante/confirmar-pagamento', participantController.confirmarPagamento); //Confirmar pagamento
-router.put('/:id_participante/cancelar-confirmacao', participantController.unconfirmPayment); //Cancelar confirmação
-router.delete('/:id_participante', participantController.deleteParticipant); //Rota para deletar um participante
-router.get('/pdf', participantController.generatePdf); //Rota para gerar PDF
+// Rota pública para criar um novo participante (sem autenticação)
+router.post('/inscricao', participantController.createPublicParticipant);
+
+// Middleware de autenticação para as rotas abaixo
+router.use(authMiddleware);
+
+// Outras rotas protegidas (que exigem autenticação)
+router.post('/', participantController.createParticipant);
+router.get('/', participantController.getAllParticipants);
+router.get('/:id_participante', participantController.getParticipantById);
+router.put('/:id_participante', participantController.updateParticipant);
+router.put('/:id_participante/confirmar-pagamento', participantController.confirmarPagamento);
+router.put('/:id_participante/cancelar-confirmacao', participantController.unconfirmPayment);
+router.delete('/:id_participante', participantController.deleteParticipant);
 
 module.exports = router;
