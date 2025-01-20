64e159a2cb8dd200100765b680d0934b7ffb7399
Thiago Souza <153232481+thiagosouza28@users.noreply.github.com>
2025-01-20 09:12:50 -0300

    Update Participant.js

--- models/Participant.js
+++ models/Participant.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const { v4: uuidv4 } = require('uuid'); // Importar a biblioteca UUID
+const { v4: uuidv4 } = require('uuid');
 
 const participantSchema = new mongoose.Schema({
     id_participante: {
         type: String,
         required: [true, 'O ID do participante é obrigatório'],
-        unique: true
+        unique: true // Manter único para cada participante
     },
     nome: {
         type: String,
@@ -17,7 +17,7 @@ const participantSchema = new mongoose.Schema({
         type: Date,
         required: [true, 'A data de nascimento é obrigatória']
     },
-    igreja: { // Referência para o modelo Igreja
+    igreja: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Igreja',
         required: [true, 'A igreja é obrigatória']
@@ -37,13 +37,13 @@ const participantSchema = new mongoose.Schema({
         type: Date,
         default: null
     },
-    uniqueId: { // ID único para permitir múltiplas inscrições com o mesmo email
+    uniqueId: {
         type: String,
         required: true,
         unique: true,
         default: uuidv4
     },
-    id_usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', default: null } // Adicione se necessário
+    id_usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', default: null } // ID do usuário (se autenticado)
 });
 
 module.exports = mongoose.model('Participant', participantSchema);
